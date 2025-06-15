e6830ae67f613cde2d45616ef7df0f56b57fde1e
Yam Borodetsky <yam@yam.codes>
2025-06-15 13:21:57 +0500

    Enhance package.json with title, description, and updated dependencies; enable JSON module resolution in tsconfig; integrate Swagger documentation in Elysia app with new routes.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
+import { description, title, version } from "../package.json";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+	.use(
+		swagger({
+			documentation: {
+				info: { title, version, description },
+			},
+			exclude: ["/"],
+		}),
+	)
+	.get("/", ({ redirect }) => redirect("/swagger"))
+	.get("/hello", () => "Hello Elysia")
+	.listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`,
 );
