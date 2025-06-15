48a7f9583f4bbe52c47f4bd9aa9af3a48bc0bed0
Yam Borodetsky <yam@yam.codes>
2025-06-15 16:42:04 +0500

    Refactor database structure by moving schema definition to db.ts; update drizzle.config.ts to reflect new schema path; remove obsolete db.ts and index.ts files.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
 	out: "./drizzle",
-	schema: "./src/db/schema.ts",
+	schema: "./src/db.ts", // Move to separate files once the project scales
 	dialect: "sqlite",
 	dbCredentials: {
 		url: env.DATABASE_URL,
--- src/db/db.ts
+++
@@ -1,4 +0,0 @@
-import env from "@env";
-import { drizzle } from "drizzle-orm/bun-sqlite";
-
-export const db = drizzle(env.DATABASE_URL);
